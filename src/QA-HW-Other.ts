d066e8e8f2ea21d8352b81ac03302bec0bd50064
Ivan Soranja <s.oranja.s@gmail.com>
2023-07-09 14:38:13 +0400

    tidied up the code and logs

--- src/QA-HW-Other.ts
+++ src/QA-HW-Other.ts
@@ -4,7 +4,6 @@ Questions:
 - split.join or replace?
 - how to BREAK the line if it won't fit??? (kinda solved)
 - how to align items to flex start???
-- how to remove the extra spase (the second slide in the bottom) (SOLVED)
 
 */
 
@@ -13,14 +12,18 @@ Questions:
 /*
 Homework:
 
-- add loader, fallback images, etc.
 - add breakpoints to slider and cards + stylize
-- 6/22/23 - mobile looks horrible, need to fix it!
-- how to remove focus animation (CardActionArea)
-- make the slider loop smoother
-- change and format data for two words
+- make the slider loop smoother (PROB: scale)
 
-- IF TIME: change top bar (Material UI or BY YOURSELF)
+For public:
+- !!! generate and add at least 5-7 categories (10 max);
+- !!! on mobile, make cards LARGER (x1.5 - 2);
+- !!! change the logo;
+- add loader, fallback images, etc.;
+- generate and add bg image with stick;
+- turn off Google functionality;
+- change the top bar (Material UI or BY YOURSELF);
+- create the description doc;
 
 */
 
@@ -29,13 +32,22 @@ Homework:
 /*
 Other:
 
-- to learn and practice theory: async code, types and interface
-- review localStorage
+- to learn and practice theory: 
+        * async code, types and interfaces;
+- review localStorage;
 
 */
 
 /*
 Code drafts:
+
+*/
+
+/*
+LOG:
+
+- 6/22/23 - mobile looks horrible, need to fix it! 
+- 7/9/23 - looks better now!
 
 */
 
--- src/constants/sliderSettings.ts
+++ src/constants/sliderSettings.ts
@@ -1,5 +1,4 @@
 export const settings = {
-  className: "slider variable-width",
   centerMode: true,
   dots: true,
   infinite: true,
@@ -7,8 +6,6 @@ export const settings = {
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
-  initialSlide: 0,
-  // variableWidth: true,
   responsive: [
     {
       breakpoint: 1400,
