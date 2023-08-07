089af0179f45c743581d6ee2ea67327f799ccf21
Soranja <s.oranja.s@gmail.com>
2023-08-07 19:08:36 +0400

    small fixes

--- src/components/categoriesSearch/CategoriesSearch.tsx
+++ src/components/categoriesSearch/CategoriesSearch.tsx
@@ -1,83 +1,10 @@
 import React from "react";
 import "./CategoriesSearch.css";
-import { TextField } from "@mui/material";
-import Autocomplete from "@mui/material/Autocomplete";
-import { categories } from "../../constants/categories";
-import { outlinedInputClasses } from "@mui/material/OutlinedInput";
-import {
-  createTheme,
-  ThemeProvider,
-  Theme,
-  useTheme,
-} from "@mui/material/styles";
+import { TextField, Autocomplete } from "@mui/material";
+import { ThemeProvider, useTheme } from "@mui/material/styles";
 
-const customTheme = (outerTheme: Theme) =>
-  createTheme({
-    palette: {
-      mode: outerTheme.palette.mode,
-    },
-    components: {
-      MuiTextField: {
-        styleOverrides: {
-          root: {
-            "--TextField-brandBorderColor": "#FFF",
-            "--TextField-brandBorderHoverColor": "#FFF",
-            "--TextField-brandBorderFocusedColor": "#FFF",
-            "& label.Mui-focused": {
-              color: "var(--TextField-brandBorderFocusedColor)",
-            },
-          },
-        },
-      },
-      MuiOutlinedInput: {
-        styleOverrides: {
-          notchedOutline: {
-            borderColor: "var(--TextField-brandBorderColor)",
-          },
-          root: {
-            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
-              borderColor: "var(--TextField-brandBorderHoverColor)",
-            },
-            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
-              borderColor: "var(--TextField-brandBorderFocusedColor)",
-            },
-          },
-        },
-      },
-      MuiFilledInput: {
-        styleOverrides: {
-          root: {
-            "&:before, &:after": {
-              borderBottom: "2px solid var(--TextField-brandBorderColor)",
-            },
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
-            },
-            "&.Mui-focused:after": {
-              borderBottom:
-                "2px solid var(--TextField-brandBorderFocusedColor)",
-            },
-          },
-        },
-      },
-      MuiInput: {
-        styleOverrides: {
-          root: {
-            "&:before": {
-              borderBottom: "2px solid var(--TextField-brandBorderColor)",
-            },
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
-            },
-            "&.Mui-focused:after": {
-              borderBottom:
-                "2px solid var(--TextField-brandBorderFocusedColor)",
-            },
-          },
-        },
-      },
-    },
-  });
+import { categories } from "../../constants/categories";
+import { customTheme } from "../../constants/muiCustomTheme";
 
 export function CategoriesSearch() {
   const outerTheme = useTheme();
@@ -86,8 +13,6 @@ export function CategoriesSearch() {
     <ThemeProvider theme={customTheme(outerTheme)}>
       <Autocomplete
         className="categories-search"
-        // disablePortal
-        // id="combo-box-demo"
         options={categories}
         noOptionsText={"No such a category yet ;)"}
         renderInput={(params) => (
@@ -96,16 +21,11 @@ export function CategoriesSearch() {
             label="Vocabulary Categories"
             variant="outlined"
             sx={{
-              "& .Mui-focused ": {
-                color: "#fff",
-              },
-              "& .MuiInputLabel-root": {
+              "& .Mui-focused, .MuiInputLabel-root": {
                 color: "#fff",
                 borderColor: "#fff",
               },
             }}
-            // InputLabelProps={{ shrink: true }}
-            // focused
           />
         )}
       />
---
+++ src/constants/muiCustomTheme.ts
@@ -0,0 +1,70 @@
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
+import { createTheme, Theme } from "@mui/material/styles";
+
+export const customTheme = (outerTheme: Theme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "--TextField-brandBorderColor": "#FFF",
+            "--TextField-brandBorderHoverColor": "#FFF",
+            "--TextField-brandBorderFocusedColor": "#FFF",
+            "& label.Mui-focused": {
+              color: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline: {
+            borderColor: "var(--TextField-brandBorderColor)",
+          },
+          root: {
+            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderHoverColor)",
+            },
+            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            "&:before, &:after": {
+              borderBottom: "2px solid var(--TextField-brandBorderColor)",
+            },
+            "&:hover:not(.Mui-disabled, .Mui-error):before": {
+              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
+            },
+            "&.Mui-focused:after": {
+              borderBottom:
+                "2px solid var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiInput: {
+        styleOverrides: {
+          root: {
+            "&:before": {
+              borderBottom: "2px solid var(--TextField-brandBorderColor)",
+            },
+            "&:hover:not(.Mui-disabled, .Mui-error):before": {
+              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
+            },
+            "&.Mui-focused:after": {
+              borderBottom:
+                "2px solid var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+    },
+  });
