bb0638e04d49b0f8d7651e018d4c249f84719694
Soranja <s.oranja.s@gmail.com>
2023-08-17 19:26:22 +0400

    minor fixes

--- src/QA-HW-Other.ts
+++ src/QA-HW-Other.ts
@@ -6,41 +6,18 @@ Questions:
 // ============================================
 
 /*
-Homework:
-
-- make NavLink component, change CSS with props - STARTED
-- autocomplete CSS styles - STARTED
-
-*/
-
-// ============================================
-
-/*
 
 Priorities:
 
 !!!:
-- fix padding on about
 - fix broken design on mobile
+- make NavLink component, change CSS with props - STARTED
 
 !!:
 - replace bootstrap.min.css OR leave it? it's hard to make it work.. (proper centered slider + full size window)
 
 !:
-- create the description doc + about page info;
 - add loader, fallback images, etc. (use default MUI);
-
-*/
-
-// ============================================
-
-/*
-
-Other:
-
-- to learn and practice theory: 
-        * async code, types and interfaces;
-- review localStorage;
 
 */
 
--- src/constants/muiCustomTheme.ts
+++ src/constants/muiCustomTheme.ts
@@ -34,37 +34,5 @@ export const customTheme = (outerTheme: Theme) =>
           },
         },
       },
-      MuiFilledInput: {
-        styleOverrides: {
-          root: {
-            "&:before, &:after": {
-              borderBottom: "2px solid var(--TextField-brandBorderColor)",
-            },
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
-            },
-            "&.Mui-focused:after": {
-              borderBottom:
-                "2px solid var(--TextField-brandBorderFocusedColor)",
-            },
-          },
-        },
-      },
-      MuiInput: {
-        styleOverrides: {
-          root: {
-            "&:before": {
-              borderBottom: "2px solid var(--TextField-brandBorderColor)",
-            },
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
-            },
-            "&.Mui-focused:after": {
-              borderBottom:
-                "2px solid var(--TextField-brandBorderFocusedColor)",
-            },
-          },
-        },
-      },
     },
   });
