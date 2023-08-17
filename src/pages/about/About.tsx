b62913defc1b3d22668911a9345ad64cce58d444
Soranja <s.oranja.s@gmail.com>
2023-08-17 19:02:46 +0400

    filled 'about'

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,10 +5,20 @@ import "./About.css";
 
 function About() {
   return (
-    <div className="about">
-      <div className="welcome" style={{ padding: 10 }}>
-        Home
-      </div>
+    <div className="about-wrapper">
+      <h2 className="about-title">
+        Welcome to my demo project <span>Learn Armenian</span>!
+      </h2>
+      <p className="about-text">
+        It is an educational app, and the main goal is quite straight: teach you
+        some Armenian! So far the demo consists of one section, Picture
+        Dictionary, where you can find some useful words and phrases with
+        explanatory illustrations.
+      </p>
+      <p className="about-text">
+        The futher sections may include: Armenian phonetics theory and
+        exercises, Grammar rules, Vocabulary and grammar exercises...
+      </p>
     </div>
   );
 }
