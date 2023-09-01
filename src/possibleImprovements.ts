4ab146a5929407817a2d0ea02e619f23e5b333ec
Soranja <s.oranja.s@gmail.com>
2023-09-01 19:11:04 +0400

    minor fix

---
+++ src/possibleImprovements.ts
@@ -0,0 +1,9 @@
+/*
+
+- replace bootstrap.min.css OR leave it? it's hard to make it work.. (proper centered slider + full size window);
+- add loader, fallback images, etc. (use default MUI);
+- add more content (words, phrases, categories)
+
+*/
+
+export default {};
